d9ef4e489bd9591af108ef6438387fede1d46b72
MarcosGomezPora <marcosgomezpora@gmail.com>
2023-01-09 20:58:19 +0100

    Iteration3

--- es6Exercises.js
+++ es6Exercises.js
@@ -35,3 +35,28 @@ console.log(itv);
 console.log(year1);
 console.log(year2);
 console.log(year3);
+
+
+//Iteration3
+const pointsList = [32, 54, 21, 64, 75, 43];
+let pointsListCopy = [...pointsList];
+console.log(pointsListCopy);
+
+const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
+let toyCopy = {...toy};
+console.log(toyCopy);
+
+const pointsList1 = [32, 54, 21, 64, 75, 43];
+const pointsLis2 = [54,87,99,65,32];
+let pointsListSum = [...pointsList1, ...pointsLis2];
+console.log(pointsListSum);
+
+const toy1 = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
+const toyUpdate = {lights: 'rgb', power: ['Volar like a dragon', 'MoonWalk']};
+let toySum = {...toy1, ...toyUpdate};
+console.log(toySum);
+
+const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];
+let colorsCopy = [...colors];
+colorsCopy.splice(2,1);
+console.log(colorsCopy);
