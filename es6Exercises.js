267cd58120afd56853f6945a9e491d41026da8ca
MarcosGomezPora <marcosgomezpora@gmail.com>
2023-01-11 16:46:34 +0100

    Iteration6

--- es6Exercises.js
+++ es6Exercises.js
@@ -7,6 +7,7 @@ suma();
 suma(5);
 suma(5, 1);
 
+
 //Iteraion2
 const game = {title: 'The last us 2', gender: ['action', 'zombie', 'survival'], year: 2020};
 let {title, gender, year} = game;
@@ -14,11 +15,13 @@ console.log(title);
 console.log(gender);
 console.log(year);
 
+
 const fruits = ['Banana', 'Strawberry', 'Orange'];
 const [fruit1, fruit2, fruit3] = fruits;
 console.log(fruit1);
 console.log(fruit2);
 console.log(fruit3);
+
 
 const animalFunction = () => {
     return {name: 'Bengal Tiger', race: 'Tiger'}
@@ -26,6 +29,7 @@ const animalFunction = () => {
 const {name, race} = animalFunction();
 console.log(name);
 console.log(race);
+
 
 const car = {name1: 'Mazda 6', itv: [2015, 2011, 2020] };
 const {name1, itv} = car;
@@ -37,29 +41,35 @@ console.log(year2);
 console.log(year3);
 
 
+
 //Iteration3
 const pointsList = [32, 54, 21, 64, 75, 43];
 let pointsListCopy = [...pointsList];
 console.log(pointsListCopy);
 
+
 const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
 let toyCopy = {...toy};
 console.log(toyCopy);
+
 
 const pointsList1 = [32, 54, 21, 64, 75, 43];
 const pointsLis2 = [54,87,99,65,32];
 let pointsListSum = [...pointsList1, ...pointsLis2];
 console.log(pointsListSum);
 
+
 const toy1 = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
 const toyUpdate = {lights: 'rgb', power: ['Volar like a dragon', 'MoonWalk']};
 let toySum = {...toy1, ...toyUpdate};
 console.log(toySum);
 
+
 const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];
 let colorsCopy = [...colors];
 colorsCopy.splice(2,1);
 console.log(colorsCopy);
+
 
 
 //Iteration4
@@ -71,6 +81,7 @@ const users = [
 ];
 const names = users.map((user) => user.name);
 console.log(names);
+
 
 const persons = [
 	{id: 1, name: 'Abel'},
@@ -85,6 +96,7 @@ const names1 = persons.map((person) => {
     return person.name;
 });
 console.log(names1);
+
 
 const cities = [
 	{isVisited:true, name: 'Tokyo'}, 
@@ -101,16 +113,19 @@ const citiesMap = cities.map((city) => {
 console.log(citiesMap);
 
 
+
 //Iteration5
 const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90];
 const agesFiltered = ages.filter((age) => age > 18);
 console.log(agesFiltered);
+
 
 const numbers = [22, 14, 24, 55, 65, 21, 12, 13, 90];
 const numbersFiltered = numbers.filter((number) => {
 	return number % 2 === 0;
 })
 console.log(numbersFiltered);
+
 
 const streamers = [
 	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
@@ -123,10 +138,12 @@ const streamersLol = streamers.filter((streamer) => {
 })
 console.log(streamersLol);
 
+
 const streamersUName = streamers.filter((streamer) => {
 	return streamer.name.includes('u');
 })
 console.log(streamersUName);
+
 
 const streamers35 = streamers.filter((streamer) => {
 	if (streamer.gameMorePlayed.includes('Legends')) {
@@ -138,7 +155,8 @@ const streamers35 = streamers.filter((streamer) => {
 })
 console.log(streamers35);
 
-const searchbar$$ = document.getElementById('inputFilter');
+
+/* const searchbar$$ = document.getElementById('inputFilter');
 const button$$ = document.querySelector('button') //Iteration 5.7
 const buscar = () => {
 	const streamersNameFilter = streamers.filter((streamer) => {
@@ -148,5 +166,44 @@ const buscar = () => {
 	console.log(streamersNameFilter);
 }
 searchbar$$.addEventListener("input", buscar);
-button$$.addEventListener("click", buscar); //Iteration5.7
+button$$.addEventListener("click", buscar);  *///Iteration5.7
 
+
+
+//Iteration6
+const numbers1 = [32, 21, 63, 95, 100, 67, 43];
+const number10 = numbers1.find((element) => {
+	return element === 100
+});
+console.log(number10);
+
+
+const movies = [
+	{title: 'Madagascar', stars: 4.5, date: 2015},
+	{title: 'Origen', stars: 5, date: 2010},
+	{title: 'Your Name', stars: 5, date: 2016}
+];
+const movies2010 = movies.find((movie) => {
+	return movie.date === 2010
+});
+console.log(movies2010);
+
+
+const aliens = [
+	{name: 'Zalamero', planet: 'Eden', age: 4029},
+	{name: 'Paktu', planet: 'Andromeda', age: 32},
+	{name: 'Cucushumushu', planet: 'Marte', age: 503021}
+];
+const mutations = [
+	{name: 'Porompompero', description: 'Hace que el alien pueda adquirir la habilidad de tocar el tambor'},
+	{name: 'Fly me to the moon', description: 'Permite volar, solo y exclusivamente a la luna'},
+	{name: 'Andando que es gerundio', description: 'Invoca a un señor mayor como Personal Trainer'}
+];
+const alienCucu = aliens.find((alien) => {
+	return alien.name === 'Cucushumushu'
+});
+const mutationPorom = mutations.find((mutation) => {
+	return mutation.name === 'Porompompero'
+});
+const fusion = {...alienCucu, mutación: { ...mutationPorom}};
+console.log(fusion);
